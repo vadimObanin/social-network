e6eacfe705b7547482bd54c8f20d8e3fdeb2b262
Vadim Obanin <mortido3@mail.ru>
2020-10-10 21:22:08 +0300

    Add components: 1. Header.jsx 2. Navbar.jsx 3. Profile.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://imgaz.staticbg.com/customers_avatars/20190711020353_451.jpg"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://img2.fonwall.ru/o/er/monitor-montagne-mountain-multi.jpeg?route=low&h=200"
+          alt=""
+        />
+      </div>
+      <div>ava + discription</div>
+      <div>My posts</div>
+      <div>New posts</div>
+      <div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+  );
+};
+export default Content;
