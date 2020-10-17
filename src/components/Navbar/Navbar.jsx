108a762bc54a2483ba701f241a9683e889daccb1
Vadim Obanin <mortido3@mail.ru>
2020-10-17 12:51:51 +0300

    019

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ const Navbar = () => {
   return (
     <nav className={classes.nav}>
       <div className={classes.item}>
-        <a>Profile</a>
+        <a href="/profile">Profile</a>
       </div>
       <div className={classes.item}>
-        <a>Messagers</a>
+        <a href="/dialogs">Messagers</a>
       </div>
       <div className={classes.item}>
-        <a>News</a>
+        <a href="/news">News</a>
       </div>
       <div className={classes.item}>
         <a>Music</a>
